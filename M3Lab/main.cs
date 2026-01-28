cc3eb14c8e9e8b7382b8bb569b68d6b4901c0780
T H <2008bluestone@gmail.com>
2026-01-28 05:19:51 -0800

    Update main.cs

--- M3Lab/main.cs
+++ M3Lab/main.cs
@@ -9,23 +9,20 @@ using System.Collections.Generic;
  */
 class Program
 {
-    //temp Employee for testing
     private class TestEmployee : Employee {
-        public TestEmployee(string name, int age, string title) : base(name, age, title, 0m) { }
+      //temp, will be replaced later
+      public TestEmployee(string name, int age, string title) : base(name, age, title, 0m) { }
     }
 
-    static void Main()
-    {
-
-        //create owner
-        Owner craig = new Owner("Craig", 50);
-
-        //create employees (testing, will change to Manager, Accountant, Blacksmith)
-        Employee john = new TestEmployee("John", 40, "Manager");
-        Employee jane = new TestEmployee("Jane", 35, "Accountant");
-        Employee jack = new TestEmployee("Jack", 30, "Blacksmith");
-
-        //owner sends messages
-        craig.SendMessage( "Good Job", new List<Employee> { john, jane, jack } );
+    static void Main() {
+      Owner craig = new Owner("Craig", 50);
+      
+      Employee john = new TestEmployee("John", 40, "Manager");
+      Employee jane = new TestEmployee("Jane", 35, "Accountant");
+      Employee jack = new TestEmployee("Jack", 30, "Blacksmith");
+      
+      var employees = new List<Employee> { john, jane, jack };
+      
+      craig.SendMessage("Good Job", employees);
     }
 }
